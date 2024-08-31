f4fd9031fd981a03047ac5afa19abca112ecaf8d
권길남 <nimpa3201@naver.com>
2024-08-31 16:50:15 +0900

    컴파일에러와 런타임에러

---
+++ ch8-1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    /* 컴파일 에러 실습
+    public static void main(String[] args) {
+        system.out.println(args[0]);
+    } */
+
+       // 런파일 에러 실습
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+    }
+
+}
